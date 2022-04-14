0690c6cec34d924cfbeee0f40a7acf19ffd577c2
Shakh204 <jalilovshakhzodkhon@gmail.com>
2022-04-14 18:39:13 -0400

    'change'

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     }, [])
     return (
       <div className="app">
-          <h1>MovieLand</h1>
+          <h1>MovieIMDB</h1>
           <div className="search">
             <input placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
             <img src={SearchIcon} alt="Search" onClick={() => {
